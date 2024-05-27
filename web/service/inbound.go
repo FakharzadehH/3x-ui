ec9dbd1c48fccf0098ba168ecbedb9d4056e6a29
Hossein Fakharzadeh <88323819+FakharzadehH@users.noreply.github.com>
2024-05-27 18:12:30 +0330

    Update inbound.go

--- web/service/inbound.go
+++ web/service/inbound.go
@@ -313,18 +313,18 @@ func (s *InboundService) UpdateInbound(inbound *model.Inbound) (*model.Inbound, 
 	}()
 
 	err = s.updateClientTraffics(tx, oldInbound, inbound)
-	if err != nil {
-		return inbound, false, err
-	}
-	if oldInbound.Up != 0 && inbound.Up != 0 {
-		oldInbound.Up = inbound.Up
-	}
-	if oldInbound.Down != 0 && inbound.Down != 0 {
-		oldInbound.Up = inbound.Up
-	}
-	if oldInbound.Total != 0 && inbound.Total != 0 {
-		oldInbound.Up = inbound.Up
-	}
+	// if err != nil {
+	// 	return inbound, false, err
+	// }
+	// if oldInbound.Up != 0 && inbound.Up != 0 {
+	// 	oldInbound.Up = inbound.Up
+	// }
+	// if oldInbound.Down != 0 && inbound.Down != 0 {
+	// 	oldInbound.Up = inbound.Up
+	// }
+	// if oldInbound.Total != 0 && inbound.Total != 0 {
+	// 	oldInbound.Up = inbound.Up
+	// }
 	// oldInbound.Up = inbound.Up
 	// oldInbound.Down = inbound.Down
 	// oldInbound.Total = inbound.Total
